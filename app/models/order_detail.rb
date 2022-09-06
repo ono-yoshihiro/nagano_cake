027ed3a6e835e1ba8090750440df4da77642ad10
EC2 Default User <ec2-user@ip-172-31-11-44.ap-northeast-1.compute.internal>
2022-09-06 16:50:16 +0000

    Add enum

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,4 +3,6 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
+  enum making_status: { cannot_produce: 0, waiting_for_production: 1, in_production:2, production_completed:3 }
+
 end
