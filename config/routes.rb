0c0fa0be4600c7281848440999be4a32810e9bb4
EC2 Default User <ec2-user@ip-172-31-11-44.ap-northeast-1.compute.internal>
2022-10-04 10:40:33 +0000

    [Start]20221004

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ post 'addresses' => 'public/addresses#create'
 get 'addresses/:id/edit' => 'public/addresses#edit', as: 'edit_addresses'
 patch 'addresses/:id' => 'public/addresses#update'
 delete 'addresses/:id' => 'public/addresses#destroy', as: 'destroy_addresses'
+get 'admin' => 'admin/homes#top'
 
 # 管理者用
   # URL /admin/sign_in ...
@@ -36,6 +37,8 @@ delete 'addresses/:id' => 'public/addresses#destroy', as: 'destroy_addresses'
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
   end
 
 # 顧客用
